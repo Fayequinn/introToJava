d02195a83d7255d46132063100726ab240fdd438
Fayequinn <fayequinn88@hotmail.com>
2023-09-08 12:32:40 +0100

    variables and numbers exercise on EX1

---
+++ src/numbers/Runner.java
@@ -0,0 +1,22 @@
+package numbers;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        System.out.println(num1+num2);
+
+        float result = (float) num1 /num2; //IJ suggestion
+        System.out.println(result);
+
+        int num3 = 8;
+        System.out.println(num3++);
+
+        System.out.println(num3);
+
+        System.out.println(++num3);
+
+
+    }
+}
---
+++ src/variables/Runner.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        String firstName = "Faye";
+        String lastName = "Quinn";
+
+        String name = firstName + lastName;
+
+        System.out.println(name);
+
+    }
+}
